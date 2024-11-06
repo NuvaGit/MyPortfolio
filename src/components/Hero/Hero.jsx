e692f1810483723f192805290cb79abd1d13dc62
NuvaGit <jack.neilan@ucdconnect.ie>
2024-11-06 15:57:23 +0000

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, Jack</h1>
+        <p className={styles.description}>
+          Im a University student set to graduate in 2025 looking for work in Tech and Financial Industry
+        </p>
+        <a href="mailto:jackneilan02@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
